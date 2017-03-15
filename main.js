7df2b568540d7ad97f45090e675660ff1b614fff
kerrongordon <kgpsounds.com@gmail.com>
2017-03-15 08:17:47 -0400

    init

--- main.js
+++ main.js
@@ -1,55 +1,83 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const { app, BrowserWindow, Tray, Menu, nativeImage } = require('electron');
+const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+require('electron-context-menu')({
+    prepend: (params, browserWindow) => [{
+        label: 'Menu',
+        // only show it when right-clicking images
+        visible: params.mediaType === 'image'
+    }]
+});
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+let win;
+let tray = null;
+let icon = path.join(__dirname, 'resources/icon/64/whatsapp.png');
+let iconTray = path.join(__dirname, 'resources/icon/16/whatsapp.png');
 
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+function createMainWindow() {
+    win = new BrowserWindow({
+        height: 600,
+        width: 900,
+        icon: icon,
+        show: false,
+        autoHideMenuBar: true
+    });
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+    win.loadURL('https://web.whatsapp.com/');
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    win.on('ready-to-show', () => {
+        win.show();
+    });
+
+    win.on('minimize', (event) => {
+        event.preventDefault();
+        win.hide();
+    });
+
+    win.on('close', (event) => {
+        if (!app.isQuiting) {
+            event.preventDefault();
+            win.hide();
+        }
+        return false;
+    });
+
+
+    // tray icon
+
+    tray = new Tray(iconTray);
+    const contextMenu = Menu.buildFromTemplate([{
+            label: 'Toggle',
+            click: () => {
+                win.isVisible() ? win.hide() : win.show()
+            }
+        },
+        { type: 'separator' },
+        {
+            label: 'Quit',
+            click: () => {
+                app.isQuiting = true;
+                app.quit();
+            }
+        }
+    ]);
+    tray.setToolTip('WhatsApp');
+    tray.setContextMenu(contextMenu);
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('ready', () => {
+    createMainWindow();
+});
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+    if (!win) {
+        createMainWindow();
+    }
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
