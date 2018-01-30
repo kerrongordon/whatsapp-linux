047cc8e34bb1ae482a994683e2ca1e25e85b7541
kerrongordon <kgpsounds.com@gmail.com>
2018-01-30 09:52:15 -0400

    init new whatsapp

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,30 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
-import * as url from "url";
+import { app, BrowserWindow } from 'electron'
 
-let mainWindow: Electron.BrowserWindow;
+let mainWindow: Electron.BrowserWindow
+const appURL = 'https://web.whatsapp.com/'
+const appName = 'WhatsApp linux'
+const iconURL = 'assets/png/64x64.png'
+const TrayIconURL = 'assets/png/16x16.png'
+const bgColor = '#f2f2f2'
 
-function createWindow() {
-  // Create the browser window.
+const createWindow = () => {
   mainWindow = new BrowserWindow({
+    backgroundColor: bgColor,
     height: 600,
-    width: 800,
-  });
+    icon: iconURL,
+    width: 900,
+  })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, "../index.html"),
-      protocol: "file:",
-      slashes: true,
-  }));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+  mainWindow.loadURL(appURL)
+  mainWindow.setTitle(appName)
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.setAutoHideMenuBar(true)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+const closeApp = () => {
+  return app.quit()
+}
 
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+app.on('ready', () => {
+  createWindow()
+})
