be4c5ea210e8ead6305c702c1b8d2b5361466efb
kerrongordon <kgpsounds.com@gmail.com>
2019-09-08 13:04:56 -0400

    remove yarn and move to npm

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ require('electron-context-menu')({
 let mainWindow: Electron.BrowserWindow
 const appURL = 'https://web.whatsapp.com/'
 const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/76.0.3809.100 Chrome/76.0.3809.100 Safari/537.36'
-const appName = 'WhatsApp linux'
+const appName = 'WhatsApp'
 const bgColor = '#f2f2f2'
 
 const createWindow = () => {
