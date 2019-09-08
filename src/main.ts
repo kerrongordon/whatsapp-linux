b75fe493fa98820cb2bf6639f569e084f3561b75
kerrongordon <kgpsounds.com@gmail.com>
2019-09-08 10:30:44 -0400

    update userAgent

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ require('electron-context-menu')({
 
 let mainWindow: Electron.BrowserWindow
 const appURL = 'https://web.whatsapp.com/'
-const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/76.0.3809.100 Chrome/76.0.3809.100 Safari/537.36'
 const appName = 'WhatsApp linux'
 const bgColor = '#f2f2f2'
 
